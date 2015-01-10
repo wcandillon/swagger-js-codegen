7a7ad12045b5a9abc74338130c4a4b8ce8bcb3a4
nick <nickfloros@gmail.com>
2015-01-10 09:10:09 +0000

    initial upload for jsdoc parsing

---
+++ tests/samples/sample_jsdoc.js
@@ -0,0 +1,36 @@
+
+
+/**
+ * LatLong position in WSG86 coordinate system
+ * @constructor LatLong
+ * @swagger
+ */
+function LatLong() {
+
+	/**
+	 * Latitude value
+	 * @type {Number} 
+	 */
+	this.lat = 0;
+	
+	/**
+	 * Longitude value
+	 * @type {Number} 
+	 */
+	this.lng=0;
+
+	/**
+	 * list of ids 
+	 * @type {List} 
+	 */
+	this.ids=[];
+
+	/**
+	 * need to have a foo value
+	 * @type {integer} 
+	 */
+	this.foo=0;
+
+}
+
+

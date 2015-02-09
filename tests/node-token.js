e0d0da7b8001d9ceb6b38b6338e67ac8debf4f9d
Boris Sirota <borisirota@gmail.com>
2015-02-09 21:58:24 +0200

    add 2 optional params to setToken in order to support sending token in custom header or query

--- tests/node-token.js
+++ tests/node-token.js
@@ -12,29 +12,53 @@ vows.describe('Test Token').addBatch({
         topic: function(){
             var swagger = JSON.parse(fs.readFileSync('tests/apis/token.json', 'UTF-8'));
             /*jshint evil:true*/
-            var Auth = eval(CodeGen.getNodeCode({ className: 'Auth', swagger: swagger }));
-            return new Auth('https://portal.28.io/api');
+            var Token = eval(CodeGen.getNodeCode({ className: 'Token', swagger: swagger }));
+            return new Token('https://portal.28.io/api');
         },
-        'Should have auth method': function(auth){
-            assert.equal(auth.auth !== undefined, true);
+        'Should have auth and getSecure methods': function(token){
+            assert.equal(token.auth !== undefined, true);
+            assert.equal(token.getSecure !== undefined, true);
         },
-        'Calling Auth method with correct password': {
-            topic: function (auth) {
+        'Calling auth method with correct password': {
+            topic: function (token) {
                 var promise = new(events.EventEmitter)();
-                auth.auth({
+                token.auth({
                     email: 'w+test@28.io',
                     password: 'foobar',
                     grant_type: 'client_credentials'
                 }).then(function(result){
-                    promise.emit('success', {result: result, auth: auth});
+                    promise.emit('success', result);
                 }, function(result){
                     promise.emit('error', result);
                 });
                 return promise;
             },
-            'Should have valid password': function (success) {
-                success.auth.setToken(success.result.body.access_token);
-                assert.equal(success.result.body.token_type, 'bearer');
+            'Should have valid password': function (result) {
+                assert.equal(result.body.token_type, 'bearer');
+            },
+            'Calling operation with token': {
+                topic: function (result) {
+                    var swagger = JSON.parse(fs.readFileSync('tests/apis/token.json', 'UTF-8'));
+                    /*jshint evil:true*/
+                    var Token = eval(CodeGen.getNodeCode({ className: 'Token', swagger: swagger }));
+
+                    var projectName = result.body.projectsMetadata[0].name;
+                    var accessToken = result.body.access_token;
+
+                    var token = new Token('https://' + projectName + '.28.io');
+                    token.setToken(accessToken, 'token', true);
+
+                    var promise = new(events.EventEmitter)();
+                    token.getSecure().then(function(result){
+                        promise.emit('success', result);
+                    }, function(result){
+                        promise.emit('error', result);
+                    });
+                    return promise;
+                },
+                'Should have unauthorized response': function (err, result) {
+                    assert.equal(err.body.message, 'Unauthorized');
+                }
             }
         }
     }

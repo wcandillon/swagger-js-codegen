a43a431af2a8f6d9eb17150619d861edd0aa8240
Alexander Krauss <alexander.krauss@qaware.de>
2015-10-04 22:13:45 +0200

    Dropped obsolete test.js.

--- test.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var CodeGen = require('./lib/codegen').CodeGen;
-
-var file = 'tests/apis/queries.json';
-var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
-var source = CodeGen.getTypeScriptDefinition({ className: 'Test', swagger: swagger });
-console.log(source);

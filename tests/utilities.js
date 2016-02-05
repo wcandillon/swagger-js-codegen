2e08e16c0ffeb9e492aea3df35e120e1494ac8e6
Michael R. Maletich <mmaletich@civisanalytics.com>
2016-02-05 12:42:30 +0000

    :new: Convert snake case names in addition to kebab case names to camelCase.

--- tests/utilities.js
+++ tests/utilities.js
@@ -15,6 +15,11 @@ vows.describe('Test Utilities').addBatch({
             assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
             assert.equal(camelCase('kebab-case'), 'kebabCase');
             assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
+        },
+        'should convert snake_case to camelCase': function(camelCase){
+            assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
+            assert.equal(camelCase('snake_case'), 'snakeCase');
+            assert.equal(camelCase('double_snake_case'), 'doubleSnakeCase');
         }
     }
 }).export(module);

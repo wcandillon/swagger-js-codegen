6bb3fc35033b3288ab93be2d9793535432e3e42d
Michael R. Maletich <mmaletich@civisanalytics.com>
2016-02-06 12:50:38 +0000

    :new: Added config variable to choose wether to convert snake_case variables to camelCase.

--- tests/utilities.js
+++ tests/utilities.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var vows = require('vows');
 var assert = require('assert');
 var rewire = require('rewire');
+var vows = require('vows');
+var _ = require('lodash');
 
 var CodeGen = rewire('../lib/codegen.js');
 
@@ -11,15 +12,38 @@ vows.describe('Test Utilities').addBatch({
         topic: function(){
             return CodeGen.__get__('camelCase');
         },
-        'should convert kebab-case to camelCase': function(camelCase){
-            assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
-            assert.equal(camelCase('kebab-case'), 'kebabCase');
-            assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
+        'by default': {
+            topic: function(camelCase) {
+                return _.curry(camelCase)({});
+            },
+            'should leave camelCase as camelCase': function(camelCase) {
+                assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
+            },
+            'should convert kebab-case to camelCase': function(camelCase) {
+                assert.equal(camelCase('kebab-case'), 'kebabCase');
+                assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
+            },
+            'should not convert snake_case to camelCase': function(camelCase) {
+                assert.equal(camelCase('snake_case'), 'snake_case');
+                assert.equal(camelCase('double_snake_case'), 'double_snake_case');
+            }
         },
-        'should convert snake_case to camelCase': function(camelCase){
-            assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
-            assert.equal(camelCase('snake_case'), 'snakeCase');
-            assert.equal(camelCase('double_snake_case'), 'doubleSnakeCase');
+        'when convertSnakeCase is true': {
+            topic: function(camelCase) {
+                return _.curry(camelCase)({convertSnakeCase: true});
+            },
+            'should leave camelCase as camelCase': function(camelCase) {
+                assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
+            },
+            'should convert kebab-case to camelCase': function(camelCase) {
+                assert.equal(camelCase('kebab-case'), 'kebabCase');
+                assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
+            },
+            'should convert snake_case to camelCase': function(camelCase) {
+                assert.equal(camelCase('snake_case'), 'snakeCase');
+                assert.equal(camelCase('double_snake_case'), 'doubleSnakeCase');
+            }
         }
+
     }
 }).export(module);

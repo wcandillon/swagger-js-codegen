e7da0b0af6c66237372530f3d0cbb4ce44aa5625
William Candillon <wcandillon@gmail.com>
2016-03-06 21:46:37 +0100

    :wrench:

--- tests/generation.js
+++ tests/generation.js
@@ -32,6 +32,14 @@ list.forEach(function(file){
             beautify: false
         });
         assert(typeof(result), 'string');
+        assert(typeof(result), 'string');
+        result = CodeGen.getTypescriptCode({
+            moduleName: 'Test',
+            className: 'Test',
+            swagger: swagger,
+            lint: false
+        });
+        assert(typeof(result), 'string');
         result = CodeGen.getCustomCode({
             moduleName: 'Test',
             className: 'Test',

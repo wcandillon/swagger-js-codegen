c5619c924834837dff74cd7d877bf2a532b075dd
Nicholas Albion <nicholas.albion@justice.nsw.gov.au>
2016-08-22 14:53:39 +1000

    restored tests for `selectTemplates`, `locateTemplate`

--- tests/utilities.js
+++ tests/utilities.js
@@ -3,50 +3,10 @@
 var assert = require('assert');
 var rewire = require('rewire');
 var vows = require('vows');
-var _ = require('lodash');
 
 var CodeGen = rewire('../lib/codegen.js');
 
 vows.describe('Test Utilities').addBatch({
-    'camelCase': {
-        topic: function(){
-            return CodeGen.__get__('camelCase');
-        },
-        'by default': {
-            topic: function(camelCase) {
-                return _.curry(camelCase)({});
-            },
-            'should leave camelCase as camelCase': function(camelCase) {
-                assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
-            },
-            'should convert kebab-case to camelCase': function(camelCase) {
-                assert.equal(camelCase('kebab-case'), 'kebabCase');
-                assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
-            },
-            'should not convert snake_case to camelCase': function(camelCase) {
-                assert.equal(camelCase('snake_case'), 'snake_case');
-                assert.equal(camelCase('double_snake_case'), 'double_snake_case');
-                assert.equal(camelCase('snake_with-kebab'), 'snake_withKebab');
-            }
-        },
-        'when convertSnakeCase is true': {
-            topic: function(camelCase) {
-                return _.curry(camelCase)({convertSnakeCase: true});
-            },
-            'should leave camelCase as camelCase': function(camelCase) {
-                assert.equal(camelCase('alreadyCamel'), 'alreadyCamel');
-            },
-            'should convert kebab-case to camelCase': function(camelCase) {
-                assert.equal(camelCase('kebab-case'), 'kebabCase');
-                assert.equal(camelCase('double-kebab-case'), 'doubleKebabCase');
-            },
-            'should convert snake_case to camelCase': function(camelCase) {
-                assert.equal(camelCase('snake_case'), 'snakeCase');
-                assert.equal(camelCase('double_snake_case'), 'doubleSnakeCase');
-                assert.equal(camelCase('snake_with-kebab'), 'snakeWithKebab');
-            }
-        }
-    },
     'selectTemplates': {
         topic: function(){
             var locateTemplate = CodeGen.__get__('locateTemplate');

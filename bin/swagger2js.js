a223fb7e73d8da3cafb94276794d4ebde24edfb2
Martin Cuchta <cuchta@bart.sk>
2018-11-19 06:24:38 +0100

    Removal of unneeded tool

--- bin/swagger2js.js
+++ bin/swagger2js.js
@@ -2,11 +2,6 @@
 var path = require('path');
 var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-var updateNotifier = require('update-notifier');
 
-//1. Update Notifier
-var pkg = require('../package.json');
-updateNotifier({packageName: pkg.name, packageVersion: pkg.version}).notify();
-
-//4. CLI Script
+// CLI Script
 require(lib + '/cli.js');

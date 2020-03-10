e2925fc601e2778e8acd565d9a6f3675af3abedd
Eric Tuvesson <eric.tuvesson@gmail.com>
2020-03-10 20:01:13 +0100

    Create tests for validateOptions

--- src/options/options.test.ts
+++ src/options/options.test.ts
@@ -1,4 +1,4 @@
-import { makeOptions } from "./options";
+import { makeOptions, validateOptions } from "./options";
 import * as Mustache from "mustache";
 import { Swagger } from "../swagger/Swagger";
 
@@ -35,5 +35,53 @@ describe("makeOptions", () => {
     const options = makeOptions(partialOptions);
 
     expect(options.className).toBe("GeneratedDataLayer");
+  });
+});
+
+describe("validateOptions", () => {
+  it("with valid options", () => {
+    const partialOptions = {
+      swagger: {} as Swagger,
+      template: {
+        class: "class-template",
+        method: "method-template"
+      }
+    };
+
+    const options = makeOptions(partialOptions);
+
+    expect(() => validateOptions(options));
+  });
+
+  it("throws when class template is not provided", () => {
+    const partialOptions = {
+      swagger: {} as Swagger,
+      template: {
+        class: "class-template",
+        method: undefined
+      }
+    };
+
+    const options = makeOptions(partialOptions);
+
+    expect(() => validateOptions(options)).toThrow(
+      'Unprovided custom template. Please use the following template: template: { class: "...", method: "...", request: "..." }'
+    );
+  });
+
+  it("throws when method template is not provided", () => {
+    const partialOptions = {
+      swagger: {} as Swagger,
+      template: {
+        class: undefined,
+        method: "method-template"
+      }
+    };
+
+    const options = makeOptions(partialOptions);
+
+    expect(() => validateOptions(options)).toThrow(
+      'Unprovided custom template. Please use the following template: template: { class: "...", method: "...", request: "..." }'
+    );
   });
 });
